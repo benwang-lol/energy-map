3631b77e5cd9e868854a17339a72a4b343ff0e52
Benjamin Wang <bzw2@duke.edu>
2023-09-10 09:15:22 -0400

    removed exposed api stuff oops

--- src/api.js
+++
@@ -1,9 +0,0 @@
-export const statesAPIOptions = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '52e1747104msh5ffc1948c8156e4p19c660jsnb6d5dd451ea7',
-		'X-RapidAPI-Host': 'us-states.p.rapidapi.com'
-	}
-};
-
-export const states_url = 'https://us-states.p.rapidapi.com/basic';
